625eccda1c42c33def83b36e98c347d515036764
xitmakes <aditech387@gmail.com>
2024-09-22 21:07:30 +0530

    Update scope.sh

--- scope.sh
+++ scope.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl https://hackerone.com/teams/$1/assets/download_csv.csv | grep *. | awk -F ',' '{print $1}'   | tee wildcards
-
+curl -s https://hackerone.com/teams/$1/assets/download_csv.csv | \
+    grep -Eo '(\*\.|www\.)[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}' | tee wildcards
